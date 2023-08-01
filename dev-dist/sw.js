dea102ae85802501a305985d8394516c2988b28c
Sanjay <sanjaybora380@gmail.com>
2023-08-01 14:31:44 +0530

    commit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.it2isqm4sfo"
+    "revision": "0.obl068ju7h"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/pages/home/studentHome/class.jsx
+++ src/pages/home/studentHome/class.jsx
@@ -1,18 +1,4 @@
-import { useState, useEffect } from "react"
-import api from '/src/api'
-
 const Class = ({_class}) => {
-
-    const [attendance,setAttendance] = useState(null)
-    const overallAttendance = ()=>{
-        api.post('/getAttendance',{class_id:_class.id})
-        .then(response=>{
-            setAttendance(response.data.attendance)
-        })
-    }
-    useEffect(()=>{
-        overallAttendance()
-    },[])
 
     return (
         <tr className='flex justify-between' key={_class._id}>
@@ -27,8 +13,8 @@ const Class = ({_class}) => {
                 </p>
             </td>
             <td className="py-3 px-5 w-1/3 border-b text-center">
-                <p className={`inline antialiased text-sm p-1 rounded-lg ${attendance<75?"bg-red-200":"bg-green-200"}`}>
-                    {attendance}
+                <p className={`inline antialiased text-sm p-1 rounded-lg ${_class.attendance<75?"bg-red-200":"bg-green-200"}`}>
+                    {_class.attendance}
                 </p>
             </td>
         </tr>
